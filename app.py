8dc972d1d677228b591cfdb38fbb356dc5b362f9
doublealogic <cras.ajrobertson@gmail.com>
2020-11-21 23:23:30 -0500

    Updated app.py file with Flask Routes

--- app.py
+++ app.py
@@ -24,3 +24,16 @@ Station = Base.classes.station
 # Flask Setup
 app = Flask(__name__)
 
+# Flask Routes
+
+@app.route("/")
+def welcome():
+    """Lists all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start>/ add a start date there in YYYY-MM-DD format<br/>"
+        f"/api/v1.0/<start>/<end>/ add both and a start and end date there in YYYY-MM-DD format"
+    )
