7dae2d9f90637192ad0aaadc67fc2746f78eb5f5
doublealogic <cras.ajrobertson@gmail.com>
2020-11-21 23:04:23 -0500

    Updated app.py file with Database and Flask Setup

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#Setup and Dependencies
+# Setup and Dependencies
 from flask import Flask, jsonify
 
 import numpy as np
@@ -7,3 +7,20 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy import create_engine, func
 from sqlalchemy.orm import Session
+
+# Database Setup
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# Reflects an existing database into a new model
+Base = automap_base()
+
+# Reflects the tables
+Base.prepare(engine, reflect=True)
+
+# Saves references to the table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Flask Setup
+app = Flask(__name__)
+
